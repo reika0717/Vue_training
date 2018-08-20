d936c1dda22d379466a3d0bc413ab8b1123a2358
平原麗夏 <hiraharareika@naganoakiraotto-no-iMac.local>
2018-08-20 13:16:15 +0900

    add purge button

--- js/main.js
+++ js/main.js
@@ -36,14 +36,19 @@
 				if(confirm('are you sure?')) {
 					this.todos.splice(index, 1);
 				}
+			},
+			purge: function(index) {
+				if(!confirm('delete finished?')) {
+					return;
+				}
+				this.todos = this.remaining;
 			}
 		},
 		computed: {
 			remaining: function() {
-				var items = this.todos.filter(function(todo) {
+				return this.todos.filter(function(todo) {
 					return !todo.isDone;
 				})
-				return items.length;
 			}
 		}
 	})
